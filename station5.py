724cde75a8efcc6f82ec4550973a34654742d42a
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-09-30 11:00:11 -0400

    Update station5.py

--- station5.py
+++ station5.py
@@ -1,1 +1,39 @@
+# Keily Drogt
+# 30 Sept 2024
+# Station 5
 
+# Task 1
+
+superheros = ["Spiderman", "Batman", "Wonder Women", "Green Lantern", "Deadpool"]
+
+
+
+
+# Task 2
+
+quiz_score = 98
+if quiz_score > 85: 
+    print("You earned a B or higher!")
+
+
+
+# Task 3
+
+quote = "I\'m tired of this grandpa"
+if quote.startswith("M"):
+    print("Your quote starts with an uppercase M")
+
+
+
+# Task 4
+
+file_name = "fall.png"
+if file_name.endswith(".png"):
+    print("Your file ends with the .png extention.")
+
+
+
+# Task 5
+is_registered = 200
+if is_registered > 100:
+  print("This is true!")
